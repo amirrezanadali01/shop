7fd177d090be0bbbd35e67cd70b12d79fbe18acc
amirrezanadali01 <amirrezanadali2001@gmail.com>
2023-11-18 23:49:02 +0330

    clean code cart

--- lib/cart_screen.dart
+++ lib/cart_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:shop/models.dart';
 import 'package:shop/provider.dart';
 
 class Cart extends StatefulWidget {
@@ -49,71 +50,96 @@ class _CartState extends State<Cart> {
             child: ListView.builder(
                 itemCount: value.productBag.length,
                 itemBuilder: (context, indext) {
-                  return Card(
-                    child: Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Row(
-                        children: [
-                          Expanded(
-                            child: Image(
-                                image: NetworkImage(
-                              value.productBag[indext].image,
-                            )),
-                          ),
-                          const SizedBox(
-                            width: 20,
-                          ),
-                          Expanded(
-                            flex: 3,
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text(
-                                  value.productBag[indext].title,
-                                  style: const TextStyle(
-                                      fontWeight: FontWeight.bold),
-                                ),
-                                const SizedBox(height: 10),
-                                Text(
-                                  value.productBag[indext].description,
-                                  maxLines: 2,
-                                  overflow: TextOverflow.ellipsis,
-                                  style: const TextStyle(
-                                    color: Colors.grey,
-                                  ),
-                                ),
-                                const SizedBox(height: 5),
-                                Row(
-                                  mainAxisAlignment:
-                                      MainAxisAlignment.spaceBetween,
-                                  children: [
-                                    Text("${value.productBag[indext].price} \$",
-                                        style: const TextStyle(
-                                            fontWeight: FontWeight.bold)),
-                                    IconButton(
-                                        onPressed: () {
-                                          value.removeProductToCard(
-                                              value.productBag[indext]);
-                                          if (value.productBag.isEmpty) {
-                                            Navigator.pop(context);
-                                          }
-                                        },
-                                        icon: const Icon(
-                                          Icons.remove,
-                                          color: Colors.red,
-                                        ))
-                                  ],
-                                )
-                              ],
-                            ),
-                          )
-                        ],
-                      ),
-                    ),
+                  ProductsModel product = value.productBag[indext];
+                  return CardOfCart(
+                    title: product.title,
+                    image: product.image,
+                    description: product.description,
+                    price: product.price.toString(),
+                    removeProduct: () {
+                      value.removeProductToCard(product);
+                      if (value.productBag.isEmpty) {
+                        Navigator.pop(context);
+                      }
+                    },
                   );
                 }),
           );
         },
+      ),
+    );
+  }
+}
+
+class CardOfCart extends StatelessWidget {
+  const CardOfCart({
+    required this.description,
+    required this.image,
+    required this.price,
+    required this.removeProduct,
+    required this.title,
+    super.key,
+  });
+
+  final String image;
+  final String title;
+  final String description;
+  final String price;
+  final Function()? removeProduct;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Row(
+          children: [
+            Expanded(
+              child: Image(
+                  image: NetworkImage(
+                image,
+              )),
+            ),
+            const SizedBox(
+              width: 20,
+            ),
+            Expanded(
+              flex: 3,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    title,
+                    style: const TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                  const SizedBox(height: 10),
+                  Text(
+                    description,
+                    maxLines: 2,
+                    overflow: TextOverflow.ellipsis,
+                    style: const TextStyle(
+                      color: Colors.grey,
+                    ),
+                  ),
+                  const SizedBox(height: 5),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text("$price \$",
+                          style: const TextStyle(fontWeight: FontWeight.bold)),
+                      IconButton(
+                          onPressed: removeProduct,
+                          icon: const Icon(
+                            Icons.remove,
+                            color: Colors.red,
+                          ))
+                    ],
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
